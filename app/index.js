52c672bcf23361455c3ad249ef17212a493c96fb
afr114 <afr114@gmail.com>
2016-03-18 18:24:23 -0700

    add components and routes

--- app/index.js
+++ app/index.js
@@ -1,1 +1,9 @@
-
+var React = require('react');
+var ReactDOM = require('react-dom');
+var routes = require('../config/route.js');
+//
+//
+ReactDOM.render(
+  routes,
+  document.getElementById('app')
+)
